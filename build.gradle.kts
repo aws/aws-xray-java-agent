56025f32a2b2b017feb4f1b45ccc2b5ad27ca43b
Steve Liu <liustve@amazon.com>
2026-04-13 16:32:54 -0700

    Fix Sonatype publishing URL for OSSRH sunset

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ val releaseTask = tasks.named("release")
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://aws.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://aws.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(System.getenv("SONATYPE_USERNAME"))
             password.set(System.getenv("SONATYPE_PASSWORD"))
         }
